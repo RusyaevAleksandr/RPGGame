a5b559852b53922c719fd6a47e07c25990afaaae
RusyaevAleksandr <rusyaev1985@yandex.ru>
2026-08-29 11:26:36 +0500

    Add project files.

---
+++ RPGGameConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RPGGameConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
