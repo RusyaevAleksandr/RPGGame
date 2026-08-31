dae91e4ca0fd7704d654477276279e7c7502354d
Александр Русяев <rusyaev1985@mail.ru>
2026-08-31 16:47:33 +0500

    Добавил монстра Гигантского паука.

---
+++ RPGGameConsoleApp/Monster/Nazgul.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPGGameConsoleApp.Monster
+{
+    public class Nazgul : IEnemy
+    {
+        /// <summary>
+        /// Броня по умолчанию Назгула
+        /// </summary>
+        private const int _armor = 10;
+        /// <summary>
+        /// Кол-во жизней по умолчанию монстра Назгула
+        /// </summary>
+        private int _health = 80;
+        /// <summary>
+        /// Имя врага, монстра
+        /// </summary>
+        public string Name => "Назгул";
+        /// <summary>
+        /// Кол-во жизней монстра Назгула
+        /// </summary>
+        public int Health => _health;
+        /// <summary>
+        /// Назгул живой или нет
+        /// </summary>
+        public bool IsAlive => _health > 0;
+        /// <summary>
+        /// Кол-во очков за убийство Назгула
+        /// </summary>
+        public int ExpReward => 70;
+        /// <summary>
+        /// Кол-во брони у Назгула
+        /// </summary>
+        public int Armor => _armor;
+        /// <summary>
+        /// Урон получаемый Назгулом
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="ignoreArmor"></param>
+        public void TakeDamage(int amount, bool ignoreArmor = false)
+        {
+            int realDamage = ignoreArmor ? amount - _armor/2 : amount - _armor;
+
+            _health -= Math.Max(realDamage, 0);
+
+            if (_health < 0)
+            {
+                _health = 0;
+            }
+        }
+    }
+}
