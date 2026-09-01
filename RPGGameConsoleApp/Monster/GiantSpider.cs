d08e9c84db0c7df36269cd4b216c5e71fb2a276b
Александр Русяев <rusyaev1985@mail.ru>
2026-09-01 13:40:22 +0500

    Добавил монстра Гигантского паука и исправил монстра Назгула.

---
+++ RPGGameConsoleApp/Monster/GiantSpider.cs
@@ -0,0 +1,50 @@
+﻿namespace RPGGameConsoleApp.Monster
+{
+    internal class GiantSpider : IEnemy
+    {
+        /// <summary>
+        /// Броня по умолчанию Гигантского Паука Шелоб
+        /// </summary>
+        private const int _armor = 8;
+        /// <summary>
+        /// Кол-во жизней по умолчанию монстра Гигантского Паука Шелоб
+        /// </summary>
+        private int _health = 110;
+        /// <summary>
+        /// Имя врага, монстра
+        /// </summary>
+        public string Name => "Гигантский Паук Шелоб";
+        /// <summary>
+        /// Кол-во жизней монстра Гигантского Паука Шелоб
+        /// </summary>
+        public int Health => _health;
+        /// <summary>
+        /// Гигантский Паук Шелоб живой или нет
+        /// </summary>
+        public bool IsAlive => _health > 0;
+        /// <summary>
+        /// Кол-во очков за убийство Гигантского Паука Шелоб
+        /// </summary>
+        public int ExpReward => 60;
+        /// <summary>
+        /// Кол-во брони у Гигантского Паука Шелоб
+        /// </summary>
+        public int Armor => _armor;
+        /// <summary>
+        /// Урон получаемый Гигантским Пауком Шелоб
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="ignoreArmor"></param>
+        public void TakeDamage(int amount, bool ignoreArmor = false)
+        {
+            int realDamage = ignoreArmor ? amount : amount - _armor;
+
+            _health -= Math.Max(realDamage, 0);
+
+            if (_health < 0)
+            {
+                _health = 0;
+            }
+        }
+    }
+}
--- RPGGameConsoleApp/Monster/Nazgul.cs
+++ RPGGameConsoleApp/Monster/Nazgul.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace RPGGameConsoleApp.Monster
+﻿namespace RPGGameConsoleApp.Monster
 {
     public class Nazgul : IEnemy
     {
